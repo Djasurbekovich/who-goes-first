1098eff8c8640e5b808933c0a1719c7f45fa121a
Djasurbekovich <elmurodziyovuddinov7@gmail.com>
2023-04-14 02:46:34 +0500

    Who Goes First ?!

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
-let elDistanceForm = document.querySelector('.form');
-let elDistanceInput = elDistanceForm.querySelector('.input');
+let elDistanceForm = document.querySelector(".form");
+let elDistanceInput = elDistanceForm.querySelector(".input");
 
-let elWalkTime = document.querySelector('.walk-time');
-let elBikeTime = document.querySelector('.bike-time');
-let elCarTime = document.querySelector('.car-time');
-let elPlaneTime = document.querySelector('.plane-time');
+let elWalkTime = document.querySelector(".walk-time");
+let elBikeTime = document.querySelector(".bike-time");
+let elCarTime = document.querySelector(".car-time");
+let elPlaneTime = document.querySelector(".plane-time");
 
-let elErrorMessage = document.querySelector('.error');
+let elErrorMessage = document.querySelector(".error");
 
 // Piyoda - 3.6 km/soat
 // Velosiped - 20 km/soat
 // Mashina - 70 km/soat
 // Samolyot - 800 km/soat
 
-let walkSpeed = 3.60;
-let bikeSpeed = 20.00;
-let carSpeed = 70.00;
-let planeSpeed = 800.00;
+let walkSpeed = 3.6;
+let bikeSpeed = 20.0;
+let carSpeed = 70.0;
+let planeSpeed = 800.0;
 
-let distance = '';
+let distance = "";
 
-
-
-elDistanceForm.addEventListener('submit', function (e) {
+elDistanceForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  
+
   distance = parseFloat(elDistanceInput.value.trim(), 10);
-  
-if (distance > 0){
+
+  if (distance > 0) {
     let walkTime = distance / walkSpeed;
     walkHour = parseInt(walkTime, 10);
-    walkMinute = Math.round((parseFloat(walkTime.toFixed(2), 10) - walkHour) * 60);
+    walkMinute = Math.round(
+      (parseFloat(walkTime.toFixed(2), 10) - walkHour) * 60
+    );
 
     let bikeTime = distance / bikeSpeed;
     bikeHour = parseInt(bikeTime, 10);
-    bikeMinute = Math.round((parseFloat(bikeTime.toFixed(2), 10) - bikeHour) * 60);
+    bikeMinute = Math.round(
+      (parseFloat(bikeTime.toFixed(2), 10) - bikeHour) * 60
+    );
 
     let carTime = distance / carSpeed;
     carHour = parseInt(carTime, 10);
@@ -42,16 +44,17 @@ if (distance > 0){
 
     let planeTime = distance / planeSpeed;
     planeHour = parseInt(planeTime, 10);
-    planeMinute = Math.round((parseFloat(planeTime.toFixed(2), 10) - planeHour) * 60);
-    
-    elErrorMessage.classList.add('d-none');
-    
+    planeMinute = Math.round(
+      (parseFloat(planeTime.toFixed(2), 10) - planeHour) * 60
+    );
+
+    elErrorMessage.classList.add("d-none");
+
     elWalkTime.textContent = `${walkHour} hours, ${walkMinute} min`;
     elBikeTime.textContent = `${bikeHour} hours, ${bikeMinute} min`;
     elCarTime.textContent = `${carHour} hours, ${carMinute} min`;
     elPlaneTime.textContent = `${planeHour} hours, ${planeMinute} min`;
-}
-else{
+  } else {
     elErrorMessage.textContent = NaN;
-}
+  }
 });
